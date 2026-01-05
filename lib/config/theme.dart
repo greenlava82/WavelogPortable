70ffbafa383fa5bb975e44ea062e319b9b6b91fd
Dan Tang <dan.tang@microchip.com>
2026-01-05 10:47:28 -0500

    Link AppTheme colors throughout

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primaryColor = Color(0xFF1565C0);
+  static const Color primaryColor = Color(0xFF0C6403);
   static const Color backgroundColor = Color(0xFFF5F5F5);
   
   static const TextStyle sectionHeader = TextStyle(
